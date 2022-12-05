5d33565348e9bec05468262434407f5a352cfc13
wangyubo <1061322005@qq.com>
2022-12-05 13:11:13 +0800

    暂存

--- competition-20221023/zs-2/solution.go
+++ competition-20221023/zs-2/solution.go
@@ -7,31 +7,32 @@ func mgcd(a int, b int) int {
 	return b
 }
 
+// 自己写的，暴力枚举
 func subarrayGCD(nums []int, k int) int {
-	gcdArray := make([]int, len(nums))
-	for i := range nums {
-		if nums[i]%k == 0 {
-			gcdArray[i] = nums[i] / k
-		}
-	}
-
 	result := 0
 	for i := range nums {
-		if gcdArray[i] == 0 {
+		if nums[i]%k != 0 {
 			continue
 		}
 
-		tmp := gcdArray[i]
-		if tmp == 1 {
+		tmp := nums[i]
+		if nums[i] == k {
+			// 本身为k就是一个子数组
 			result++
 		}
+		// 向后遍历查看有几个子数组
 		for j := i + 1; j < len(nums); j++ {
-			if gcdArray[j] == 0 {
+			if nums[j]%k != 0 {
+				// 数组中存在一个不是k的倍数的值，就不用继续找了
 				break
 			}
 
-			tmp = mgcd(tmp, gcdArray[j])
-			if tmp == 1 {
+			// 倍数的最大公约数向后传递，如 gcd(8, 4) = 4 => gcd(8, 4, 6) = gcd(4, 6) = 2
+			if tmp != k {
+				tmp = mgcd(tmp, nums[j])
+			}
+
+			if tmp == k {
 				result++
 			}
 		}
